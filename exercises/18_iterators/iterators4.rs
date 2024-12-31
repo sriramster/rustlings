0ed51eeb75f6d2a6a0c93f5acb3a51a157c0a527
Sriram Raghunathan <sriram@sequence>
2024-12-30 23:30:14 -0800

    iterators and error handling
    Signed-off-by: Sriram Raghunathan <sriram@sequence>

--- exercises/18_iterators/iterators4.rs
+++ exercises/18_iterators/iterators4.rs
@@ -1,15 +1,21 @@
+// TODO: Complete this function to return the factorial of `num` which is
+// defined as `1 * 2 * 3 * … * num`.
+// https://en.wikipedia.org/wiki/Factorial
+//
+// Do not use:
+// - early returns (using the `return` keyword explicitly)
+// Try not to use:
+// - imperative style loops (for/while)
+// - additional variables
+// For an extra challenge, don't use:
+// - recursion
 fn factorial(num: u64) -> u64 {
-    // TODO: Complete this function to return the factorial of `num` which is
-    // defined as `1 * 2 * 3 * … * num`.
-    // https://en.wikipedia.org/wiki/Factorial
-    //
-    // Do not use:
-    // - early returns (using the `return` keyword explicitly)
-    // Try not to use:
-    // - imperative style loops (for/while)
-    // - additional variables
-    // For an extra challenge, don't use:
-    // - recursion
+    if num == 0 {
+        return 1;
+    }
+        
+    let v:Vec<u64> = (1..num+1).collect();
+    v.iter().copied().reduce(|a, b| a*b).unwrap()
 }
 
 fn main() {
--- exercises/18_iterators/iterators5.rs
+++ exercises/18_iterators/iterators5.rs
@@ -28,6 +28,13 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // `map` is a hash map with `String` keys and `Progress` values.
     // map = { "variables1": Complete, "from_str": None, … }
+    let mut count = 0;
+    for val in map.values() {
+        if *val == value {
+            count += 1;
+        }
+    }
+    count
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -48,6 +55,15 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // `collection` is a slice of hash maps.
     // collection = [{ "variables1": Complete, "from_str": None, … },
     //               { "variables2": Complete, … }, … ]
+    let mut count = 0;
+    for map in collection {
+        for val in map.values() {
+            if *val == value {
+                count += 1;
+            }
+        }
+    }
+    count
 }
 
 fn main() {
